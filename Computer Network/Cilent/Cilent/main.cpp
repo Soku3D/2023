6505ceb1e59c8cc377da359f24b60e2a36d69229
Soku3D <sonwon39@naver.com>
2023-11-20 22:15:56 +0900

    Update Server

--- Computer Network/Cilent/Cilent/main.cpp
+++ Computer Network/Cilent/Cilent/main.cpp
@@ -1,11 +1,11 @@
-
+#include "WinSock2.h"
 #include <iostream>
 #include <cstdio>
 #include "Utils.h"
 #include "UDPSocket.h"
 #include "SocketAddress.h"
-using namespace server;
-using UDPSocketPtr = std::shared_ptr<UDPSocket>;
+
+using namespace  server;
 int main()
 {
 	WSADATA wsadata;
@@ -14,12 +14,12 @@ int main()
 	uint32_t nadr = inet_addr("192.168.0.104");
 	SocketAddress s = SocketAddress(ntohl(nadr), 883);
 	SocketAddress serverAddr = SocketAddress(ntohl(nadr), 3301);
-	std::shared_ptr<UDPSocket> socket = Utils::CreateUDPSocket();
 
-	socket->Bind(s);
+	std::shared_ptr<UDPSocket> udpPtr = Utils::CreateUDPSocket();
+	udpPtr->Bind(s);
 	SocketAddress clientAddr;
 	char buffer[100] = "123";
-	int t = socket->SendTo(buffer, 100, serverAddr);
+	int t = udpPtr->SendTo(buffer, 100, serverAddr);
 	std::cout << t;
 
 
